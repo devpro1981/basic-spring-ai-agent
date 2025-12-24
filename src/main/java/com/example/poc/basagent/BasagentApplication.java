3b876ccb191886a9865619a520ced73a65674f75
msalima <salim.ali@lexmark.com>
2025-12-24 12:18:19 +0530

    Basic project structure

---
+++ src/main/java/com/example/poc/basagent/BasagentApplication.java
@@ -0,0 +1,13 @@
+package com.example.poc.basagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasagentApplication.class, args);
+	}
+
+}
